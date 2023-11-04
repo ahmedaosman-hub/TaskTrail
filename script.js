a686c7339f71d27d36438945e9a8ba3a4ea88e56
Ahmed Osman <ahmedaosman00@gmail.com>
2023-11-04 12:01:37 -0700

    Fixing the display of categories and tasks.

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // Getting reference to DOM elements
-const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
+const inputBox = document.getElementById("task-input");
+const listContainer = document.getElementById("list-tasks");
 const listCategories = document.getElementById("list-categories");
 
-let currentList = "default"; // The name of the currently active task list
+let currentCategory = "default"; // The name of the currently active category
 
-// Switches to a specified task list and refreshes the view
-function switchList(listName) {
-  currentList = listName;
-  showTask();
+// Switches to a specified category and refreshes the view
+function switchCategory(categoryName) {
+  currentCategory = categoryName;
+  showTasks();
 }
 
-// Add a new task to the active list
+// Add a new task to the active category
 function addTask() {
   if (inputBox.value.trim() === "") {
     alert("Please enter a task.");
@@ -53,7 +53,7 @@ listContainer.addEventListener(
 function saveData(dataToUpdate) {
   const data = dataToUpdate || loadData();
   if (!dataToUpdate) {
-    data[currentList] = Array.from(listContainer.children).map(
+    data[currentCategory] = Array.from(listContainer.children).map(
       (li) => li.firstChild.textContent
     );
   }
@@ -66,11 +66,11 @@ function loadData() {
   return storedData ? JSON.parse(storedData) : {};
 }
 
-// Display tasks of the active list
-function showTask() {
+// Display tasks of the active category
+function showTasks() {
   listContainer.innerHTML = "";
   const data = loadData();
-  const tasks = data[currentList] || [];
+  const tasks = data[currentCategory] || [];
 
   tasks.forEach((taskName) => {
     let li = document.createElement("li");
@@ -89,11 +89,11 @@ function showTask() {
   const data = loadData();
   // Populate categories and tasks from stored data
   categories = Object.keys(data); // Assumes keys in data are category names
-  if (data[currentList]) {
-    tasks = data[currentList];
+  if (data[currentCategory]) {
+    tasks = data[currentCategory];
   }
   updateCategoriesDisplay();
-  updateTaskDisplay();
+  showTasks();
 })();
 
 // Open the new category modal
@@ -131,7 +131,7 @@ function addCategoryToList(categoryName) {
   var li = document.createElement("li");
   li.textContent = categoryName;
   li.onclick = function () {
-    switchList(categoryName);
+    switchCategory(categoryName);
   };
   ul.appendChild(li);
 }
@@ -179,11 +179,47 @@ function deleteSelectedCategory() {
     }
   });
 
-  // Switch to default list if deleted category was active
-  if (currentList === selectedCategory) {
-    currentList = "default";
-    showTask();
+  // Switch to default category if deleted category was active
+  if (currentCategory === selectedCategory) {
+    currentCategory = "default";
+    showTasks();
   }
 
   closeDeleteModal();
+}
+
+// Function to update the display of categories
+function updateCategoriesDisplay() {
+  const data = loadData();
+  const categoriesList = document.getElementById("list-categories");
+
+  // Clear existing categories in the display
+  categoriesList.innerHTML = "";
+
+  // Iterate through the categories in local storage and add them to the display
+  for (let categoryName in data) {
+    addCategoryToList(categoryName);
+  }
+}
+
+// Function to update the display of tasks for the selected category
+function updateTaskDisplay() {
+  const data = loadData();
+  const tasksList = document.getElementById("list-tasks");
+  tasksList.innerHTML = "";
+
+  // Get tasks for the currently selected category (currentList)
+  const tasks = data[currentList] || [];
+
+  // Iterate through tasks and add them to the display
+  tasks.forEach((taskName) => {
+    let li = document.createElement("li");
+    li.innerHTML = taskName;
+
+    let span = document.createElement("span");
+    span.innerHTML = "\u00D7";
+    li.appendChild(span);
+
+    tasksList.appendChild(li);
+  });
 }
